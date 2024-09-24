4f64ac793c2fb5ac44d78f39aa5ff2b56a6465e3
navanijuni <93343314+navanijuni@users.noreply.github.com>
2024-09-24 17:36:30 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,8 +1,6 @@
 hello this filecontain the AD connection logic 
-<<<<<<< HEAD
-Adde
+
 updating this file to development branch
 d some contents
 hello
-=======
->>>>>>> parent of 99ebd29 (updated)
+
